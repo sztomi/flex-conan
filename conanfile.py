d4aff7918e46d0320482064ee7f01a5db7bc81a9
sztomi <szelei.t@gmail.com>
2017-01-05 23:45:26 +0100

    Initial commit

--- conanfile.py
+++ conanfile.py
@@ -3,33 +3,41 @@ from glob import glob
 import os
 
 
-class M4Conan(ConanFile):
-    name = "m4"
-    version = "latest"
-    license = "GNU m4"
-    url = "https://github.com/sztomi/m4-conan.git"
-    description = "This is a tooling package for GNU m4"
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "virtualenv"
-
-    tarball_url = "https://gnu.cu.be/m4/m4-latest.tar.gz"
+class FlexConan(ConanFile):
+    name = 'flex'
+    version = '2.6.3'
+    license = 'MIT'
+    url = 'https://github.com/sztomi/flex-conan.git'
+    description = 'This is a tooling package for GNU flex'
+    settings = 'os', 'compiler', 'build_type', 'arch'
+    generators = 'cmake', 'virtualenv'
+    requires = (
+        'autoconf/2.69@sztomi/testing',
+        'automake/1.15@sztomi/testing',
+        'm4/latest@sztomi/testing',
+        'bison/3.0.4@sztomi/testing',
+    )
 
     def source(self):
+        self.tarball_url = 'https://github.com/westes/flex/releases/download/v{}/flex-{}.tar.gz'.format(self.version, self.version)
         tgz = self.tarball_url.split('/')[-1]
         tools.download(self.tarball_url, tgz)
         tools.untargz(tgz)
         os.unlink(tgz)
-        self.dirname = glob('m4-*')[0]
 
     def build(self):
+        self.dirname = glob('flex-*')[0]
         os.chdir(self.dirname)
-        self.run('./configure --prefix={}'.format(self.package_folder))
-        self.run('make')
+        def run_in_env(cmd):
+            activate = '. ../activate.sh && '
+            self.run(activate + cmd)
+        run_in_env('./configure --prefix={}'.format(self.package_folder))
+        run_in_env('make')
         self.run('make install')
 
     def package(self):
         pass
 
     def package_info(self):
-        self.env_info.path.append(os.path.join(self.package_folder, "bin"))
+        self.env_info.path.append(os.path.join(self.package_folder, 'bin'))
         
--- test_package/build/activate.sh
+++
@@ -1,4 +0,0 @@
-export OLD_PS1="$PS1"
-export PS1="(test_package) $PS1"
-export PATH="/home/sztomi/.conan/data/m4/latest/sztomi/testing/package/75afd4a5a43dc297f65bd5338e9887b268cb97ba/bin":$PATH
-
